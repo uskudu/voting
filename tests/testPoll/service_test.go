61644ad2c85f63f6b0709b05005cd753d601fcd8
uskudu <usskudu@gmail.com>
2025-09-25 11:45:36 +0300

    service get by id

--- tests/testPoll/service_test.go
+++ tests/testPoll/service_test.go
@@ -72,3 +72,26 @@ func TestGetPollsService(t *testing.T) {
 	require.Equal(t, "test get polls 2", polls[1].Title)
 }
 
+func TestGetPollByID(t *testing.T) {
+	service := setupService(t)
+
+	// todo create it without using service.CreatePoll
+	title := "test get poll by id"
+	options := []poll.Option{
+		poll.Option{
+			Text: "a",
+		},
+		poll.Option{
+			Text: "b",
+		},
+	}
+	err := service.CreatePoll(title, options)
+	require.NoError(t, err)
+
+	poll, err := service.GetPollByID("1")
+	require.NoError(t, err)
+	require.Equal(t, 1, poll.ID)
+	require.Equal(t, "test get poll by id", poll.Title)
+	require.Len(t, poll.Options, 2, "poll should have 2 options")
+
+}

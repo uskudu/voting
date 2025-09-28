18996d95d6da31d14e8499e231823f2805e125e3
uskudu <usskudu@gmail.com>
2025-09-28 16:05:40 +0300

    user service redi

--- internal/user/service.go
+++ internal/user/service.go
@@ -1,1 +1,38 @@
 package user
+
+type ServiceIface interface {
+	CreateUser(username string) error
+	GetUsers() ([]User, error)
+	GetUserByID(id string) (User, error)
+	UpdateUser(id, newUsername string) error
+	DeleteUser(id string) error
+}
+
+type userService struct {
+	repo RepositoryIface
+}
+
+func NewUserService(r RepositoryIface) ServiceIface {
+	return &userService{repo: r}
+}
+func (s *userService) CreateUser(username string) error {
+	user := User{
+		Username: username,
+	}
+	if err := s.repo.CreateUser(&user); err != nil {
+		return err
+	}
+	return nil
+}
+func (s *userService) GetUsers() ([]User, error) {
+	return s.repo.GetUsers()
+}
+func (s *userService) GetUserByID(id string) (User, error) {
+	return s.repo.GetUserByID(id)
+}
+func (s *userService) UpdateUser(id, newUsername string) error {
+	return s.repo.UpdateUser(id, newUsername)
+}
+func (s *userService) DeleteUser(id string) error {
+	return s.repo.DeleteUser(id)
+}
